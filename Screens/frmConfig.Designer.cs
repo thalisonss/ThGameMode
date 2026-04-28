761c5b10f1634fd1165cda294eb2f3318bc82035
Thalison Santos <thalisondssantos@gmail.com>
2026-04-27 21:02:40 -0300

    Adiciona novos componentes e melhora a interface
    Foram adicionados um `GroupBox`, `DataGridView` para serviços abertos e fechados, e novos campos de texto para pesquisa. Ajustes nas propriedades de ancoragem e tamanho melhoraram a disposição e responsividade da interface. Pesos de preenchimento de colunas de `DataGridView` foram ajustados e componentes duplicados foram removidos.

--- Screens/frmConfig.Designer.cs
+++ Screens/frmConfig.Designer.cs
@@ -48,16 +48,17 @@
             btnSave = new Button();
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            groupBox1 = new GroupBox();
             txtSearchServiceOpenClose = new TextBox();
+            dgvListServicesOpenCloseAdded = new DataGridView();
+            dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
+            dataGridViewImageColumn2 = new DataGridViewImageColumn();
             button1 = new Button();
+            txtSearchServiceOpenCloseAdded = new TextBox();
             dgvListServicesOpenClose = new DataGridView();
             dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
             dataGridViewImageColumn1 = new DataGridViewImageColumn();
             btnRefreshOpenClose = new Button();
-            txtSearchServiceOpenCloseAdded = new TextBox();
-            dgvListServicesOpenCloseAdded = new DataGridView();
-            dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
-            dataGridViewImageColumn2 = new DataGridViewImageColumn();
             cboPowerPlanClosedApp = new ComboBox();
             lblPowerPlanClosedApp = new Label();
             lblPowerPlanOpenApp = new Label();
@@ -70,16 +71,15 @@
             pararServicoToolStripMenuItem = new ToolStripMenuItem();
             abrirConfiguracoesToolStripMenuItem = new ToolStripMenuItem();
             sairToolStripMenuItem = new ToolStripMenuItem();
-            groupBox1 = new GroupBox();
             ((System.ComponentModel.ISupportInitialize)nudCheckInterval).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dgvListServicesAdded).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dgvListServices).BeginInit();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvListServicesOpenClose).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dgvListServicesOpenCloseAdded).BeginInit();
-            contextMenuStrip.SuspendLayout();
             groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvListServicesOpenCloseAdded).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvListServicesOpenClose).BeginInit();
+            contextMenuStrip.SuspendLayout();
             SuspendLayout();
             // 
             // btnInstallUninstall
@@ -113,9 +113,10 @@
             // 
             dgvListServicesAdded.AllowUserToAddRows = false;
             dgvListServicesAdded.AllowUserToDeleteRows = false;
+            dgvListServicesAdded.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            resources.ApplyResources(dgvListServicesAdded, "dgvListServicesAdded");
             dgvListServicesAdded.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvListServicesAdded.Columns.AddRange(new DataGridViewColumn[] { dgvColumnNameAdded, dgvColumnDeleteAdded });
-            resources.ApplyResources(dgvListServicesAdded, "dgvListServicesAdded");
             dgvListServicesAdded.Name = "dgvListServicesAdded";
             dgvListServicesAdded.ReadOnly = true;
             dgvListServicesAdded.CellClick += dgvListServicesAdded_CellClick;
@@ -123,12 +124,14 @@
             // dgvColumnNameAdded
             // 
             resources.ApplyResources(dgvColumnNameAdded, "dgvColumnNameAdded");
+            dgvColumnNameAdded.FillWeight = 85F;
             dgvColumnNameAdded.Name = "dgvColumnNameAdded";
             dgvColumnNameAdded.ReadOnly = true;
             // 
             // dgvColumnDeleteAdded
             // 
             resources.ApplyResources(dgvColumnDeleteAdded, "dgvColumnDeleteAdded");
+            dgvColumnDeleteAdded.FillWeight = 15F;
             dgvColumnDeleteAdded.Name = "dgvColumnDeleteAdded";
             dgvColumnDeleteAdded.ReadOnly = true;
             // 
@@ -142,9 +145,10 @@
             // 
             dgvListServices.AllowUserToAddRows = false;
             dgvListServices.AllowUserToDeleteRows = false;
+            dgvListServices.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            resources.ApplyResources(dgvListServices, "dgvListServices");
             dgvListServices.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvListServices.Columns.AddRange(new DataGridViewColumn[] { dgvColumnName, dgvColumnAdd });
-            resources.ApplyResources(dgvListServices, "dgvListServices");
             dgvListServices.Name = "dgvListServices";
             dgvListServices.ReadOnly = true;
             dgvListServices.CellClick += dgvListServices_CellClick;
@@ -152,12 +156,14 @@
             // dgvColumnName
             // 
             resources.ApplyResources(dgvColumnName, "dgvColumnName");
+            dgvColumnName.FillWeight = 85F;
             dgvColumnName.Name = "dgvColumnName";
             dgvColumnName.ReadOnly = true;
             // 
             // dgvColumnAdd
             // 
             resources.ApplyResources(dgvColumnAdd, "dgvColumnAdd");
+            dgvColumnAdd.FillWeight = 15F;
             dgvColumnAdd.Name = "dgvColumnAdd";
             dgvColumnAdd.ReadOnly = true;
             // 
@@ -188,8 +194,8 @@
             // 
             // tabControl1
             // 
-            tabControl1.Controls.Add(tabPage1);
             resources.ApplyResources(tabControl1, "tabControl1");
+            tabControl1.Controls.Add(tabPage1);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
             // 
@@ -213,10 +219,47 @@
             tabPage1.Name = "tabPage1";
             tabPage1.UseVisualStyleBackColor = true;
             // 
+            // groupBox1
+            // 
+            resources.ApplyResources(groupBox1, "groupBox1");
+            groupBox1.Controls.Add(txtSearchServiceOpenClose);
+            groupBox1.Controls.Add(dgvListServicesOpenCloseAdded);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(txtSearchServiceOpenCloseAdded);
+            groupBox1.Controls.Add(dgvListServicesOpenClose);
+            groupBox1.Controls.Add(btnRefreshOpenClose);
+            groupBox1.Name = "groupBox1";
+            groupBox1.TabStop = false;
+            // 
             // txtSearchServiceOpenClose
             // 
             resources.ApplyResources(txtSearchServiceOpenClose, "txtSearchServiceOpenClose");
             txtSearchServiceOpenClose.Name = "txtSearchServiceOpenClose";
+            // 
+            // dgvListServicesOpenCloseAdded
+            // 
+            dgvListServicesOpenCloseAdded.AllowUserToAddRows = false;
+            dgvListServicesOpenCloseAdded.AllowUserToDeleteRows = false;
+            dgvListServicesOpenCloseAdded.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            resources.ApplyResources(dgvListServicesOpenCloseAdded, "dgvListServicesOpenCloseAdded");
+            dgvListServicesOpenCloseAdded.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvListServicesOpenCloseAdded.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn2, dataGridViewImageColumn2 });
+            dgvListServicesOpenCloseAdded.Name = "dgvListServicesOpenCloseAdded";
+            dgvListServicesOpenCloseAdded.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            resources.ApplyResources(dataGridViewTextBoxColumn2, "dataGridViewTextBoxColumn2");
+            dataGridViewTextBoxColumn2.FillWeight = 85F;
+            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewImageColumn2
+            // 
+            resources.ApplyResources(dataGridViewImageColumn2, "dataGridViewImageColumn2");
+            dataGridViewImageColumn2.FillWeight = 15F;
+            dataGridViewImageColumn2.Name = "dataGridViewImageColumn2";
+            dataGridViewImageColumn2.ReadOnly = true;
             // 
             // button1
             // 
@@ -224,25 +267,33 @@
             button1.Name = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // txtSearchServiceOpenCloseAdded
+            // 
+            resources.ApplyResources(txtSearchServiceOpenCloseAdded, "txtSearchServiceOpenCloseAdded");
+            txtSearchServiceOpenCloseAdded.Name = "txtSearchServiceOpenCloseAdded";
+            // 
             // dgvListServicesOpenClose
             // 
             dgvListServicesOpenClose.AllowUserToAddRows = false;
             dgvListServicesOpenClose.AllowUserToDeleteRows = false;
+            dgvListServicesOpenClose.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            resources.ApplyResources(dgvListServicesOpenClose, "dgvListServicesOpenClose");
             dgvListServicesOpenClose.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvListServicesOpenClose.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewImageColumn1 });
-            resources.ApplyResources(dgvListServicesOpenClose, "dgvListServicesOpenClose");
             dgvListServicesOpenClose.Name = "dgvListServicesOpenClose";
             dgvListServicesOpenClose.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn1
             // 
             resources.ApplyResources(dataGridViewTextBoxColumn1, "dataGridViewTextBoxColumn1");
+            dataGridViewTextBoxColumn1.FillWeight = 85F;
             dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
             dataGridViewTextBoxColumn1.ReadOnly = true;
             // 
             // dataGridViewImageColumn1
             // 
             resources.ApplyResources(dataGridViewImageColumn1, "dataGridViewImageColumn1");
+            dataGridViewImageColumn1.FillWeight = 15F;
             dataGridViewImageColumn1.Name = "dataGridViewImageColumn1";
             dataGridViewImageColumn1.ReadOnly = true;
             // 
@@ -251,33 +302,6 @@
             resources.ApplyResources(btnRefreshOpenClose, "btnRefreshOpenClose");
             btnRefreshOpenClose.Name = "btnRefreshOpenClose";
             btnRefreshOpenClose.UseVisualStyleBackColor = true;
-            // 
-            // txtSearchServiceOpenCloseAdded
-            // 
-            resources.ApplyResources(txtSearchServiceOpenCloseAdded, "txtSearchServiceOpenCloseAdded");
-            txtSearchServiceOpenCloseAdded.Name = "txtSearchServiceOpenCloseAdded";
-            // 
-            // dgvListServicesOpenCloseAdded
-            // 
-            dgvListServicesOpenCloseAdded.AllowUserToAddRows = false;
-            dgvListServicesOpenCloseAdded.AllowUserToDeleteRows = false;
-            dgvListServicesOpenCloseAdded.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvListServicesOpenCloseAdded.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn2, dataGridViewImageColumn2 });
-            resources.ApplyResources(dgvListServicesOpenCloseAdded, "dgvListServicesOpenCloseAdded");
-            dgvListServicesOpenCloseAdded.Name = "dgvListServicesOpenCloseAdded";
-            dgvListServicesOpenCloseAdded.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            resources.ApplyResources(dataGridViewTextBoxColumn2, "dataGridViewTextBoxColumn2");
-            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            dataGridViewTextBoxColumn2.ReadOnly = true;
-            // 
-            // dataGridViewImageColumn2
-            // 
-            resources.ApplyResources(dataGridViewImageColumn2, "dataGridViewImageColumn2");
-            dataGridViewImageColumn2.Name = "dataGridViewImageColumn2";
-            dataGridViewImageColumn2.ReadOnly = true;
             // 
             // cboPowerPlanClosedApp
             // 
@@ -342,18 +366,6 @@
             sairToolStripMenuItem.Name = "sairToolStripMenuItem";
             resources.ApplyResources(sairToolStripMenuItem, "sairToolStripMenuItem");
             // 
-            // groupBox1
-            // 
-            groupBox1.Controls.Add(txtSearchServiceOpenClose);
-            groupBox1.Controls.Add(dgvListServicesOpenCloseAdded);
-            groupBox1.Controls.Add(button1);
-            groupBox1.Controls.Add(txtSearchServiceOpenCloseAdded);
-            groupBox1.Controls.Add(dgvListServicesOpenClose);
-            groupBox1.Controls.Add(btnRefreshOpenClose);
-            resources.ApplyResources(groupBox1, "groupBox1");
-            groupBox1.Name = "groupBox1";
-            groupBox1.TabStop = false;
-            // 
             // frmConfig
             // 
             resources.ApplyResources(this, "$this");
@@ -370,11 +382,11 @@
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvListServicesOpenClose).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dgvListServicesOpenCloseAdded).EndInit();
-            contextMenuStrip.ResumeLayout(false);
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvListServicesOpenCloseAdded).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvListServicesOpenClose).EndInit();
+            contextMenuStrip.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
